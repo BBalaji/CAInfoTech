8c65fa29c0511960d68535092ea9ecfd53a5d5b9
BABESTA <capgemini.besta@daimler.com>
2020-12-14 21:20:10 +0530

    Initial Commit

---
+++ RealmFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmFirstApp
+//
+//  Created by Besta, Balaji (623-Extern) on 14/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
